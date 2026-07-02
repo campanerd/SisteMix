5aa130fcfb86eef3da9026ce713b7cee3f1b3316
Davi Campaner <campanerdavi@gmail.com>
2026-07-02 15:51:08 -0300

    fix: add leading slash to RequestMapping in InstallmentController

--- backend/src/main/java/org/siste/mix/installment/web/InstallmentController.java
+++ backend/src/main/java/org/siste/mix/installment/web/InstallmentController.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("installments")
+@RequestMapping("/installments")
 public class InstallmentController {
 
     @Autowired
