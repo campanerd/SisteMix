f1835085721187c4f71bfd052221d5b2631902c8
Davi Campaner <campanerdavi@gmail.com>
2026-07-03 12:40:18 -0300

    feat: add Swagger annotations to ClientController

--- backend/src/main/java/org/siste/mix/client/web/ClientController.java
+++ backend/src/main/java/org/siste/mix/client/web/ClientController.java
@@ -1,5 +1,6 @@
 package org.siste.mix.client.web;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.siste.mix.client.dto.CreateClientRequest;
@@ -24,6 +25,7 @@ public class ClientController {
     private ClientService service;
 
     @PostMapping
+    @Operation(summary = "Cadastra um novo cliente")
     public ResponseEntity<ClientResponse> create(@RequestBody @Valid CreateClientRequest data, UriComponentsBuilder uriBuilder) {
         var client = service.create(data);
         var uri = uriBuilder.path("/{id}").buildAndExpand(client.getId()).toUri();
@@ -31,22 +33,26 @@ public class ClientController {
     }
 
     @GetMapping
+    @Operation(summary = "Listar clientes")
     public ResponseEntity<Page<ClientSummary>> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable) {
         return ResponseEntity.ok(service.list(pageable));
     }
 
     @PutMapping
+    @Operation(summary = "Atualizar um cliente")
     public ResponseEntity<ClientResponse> update(@RequestBody @Valid UpdateClientRequest data) {
         return ResponseEntity.ok(service.update(data));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Excluir um cliente")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Listar um cliente por id")
     public ResponseEntity<ClientResponse> findById(@PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
     }
