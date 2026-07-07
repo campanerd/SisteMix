1bacf04b7399fea3e8e2d33684248da1fc875f2b
Davi Campaner <campanerdavi@gmail.com>
2026-07-07 10:55:03 -0300

    perf: add entity graph to fix N+1 queries on installment listing

--- backend/src/main/java/org/siste/mix/installment/repository/InstallmentRepository.java
+++ backend/src/main/java/org/siste/mix/installment/repository/InstallmentRepository.java
@@ -1,8 +1,16 @@
 package org.siste.mix.installment.repository;
 
 import org.siste.mix.installment.model.Installment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
 
 public interface InstallmentRepository extends JpaRepository<Installment, Long>, JpaSpecificationExecutor<Installment> {
+
+    @EntityGraph(attributePaths = {"order", "order.client"})
+    @Override
+    List<Installment> findAll(Specification<Installment> spec);
 }
