565bf18600c192663b3b193c9f263698b7358c61
Davi Campaner <campanerdavi@gmail.com>
2026-07-08 12:08:49 -0300

    feat: add UserRepository with findByEmail

---
+++ backend/src/main/java/org/siste/mix/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.siste.mix.user.repository;
+
+import org.siste.mix.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByEmail(String email);
+}
