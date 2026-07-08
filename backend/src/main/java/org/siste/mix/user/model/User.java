b9a7c9d80d22046c94a04bcde2046b167e8db447
Davi Campaner <campanerdavi@gmail.com>
2026-07-08 12:08:34 -0300

    feat: add User entity implementing UserDetails

---
+++ backend/src/main/java/org/siste/mix/user/model/User.java
@@ -0,0 +1,64 @@
+package org.siste.mix.user.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.siste.mix.user.dto.CreateUserRequest;
+import org.siste.mix.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private Boolean active;
+
+    public User(CreateUserRequest data, String hashedPassword) {
+        this.name = data.name();
+        this.email = data.email();
+        this.password = hashedPassword;
+        this.role = data.role();
+        this.active = true;
+    }
+
+    public void deactivate() {
+        this.active = false;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return Boolean.TRUE.equals(active);
+    }
+}
